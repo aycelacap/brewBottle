7f767dc61745f741a49da49efa2889356afdc79e
ayce <raycel5lacap@gmail.com>
2020-11-24 17:15:52 -0800

    Migrate carts

---
+++ db/migrate/20201123232330_create_cart_items.rb
@@ -0,0 +1,14 @@
+class CreateCartItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cart_items do |t|
+      t.integer :user_id, null: false
+      t.integer :product_id, null: false
+      t.integer :quantity
+      t.timestamps
+    end
+
+    add_index :cart_items, :user_id, :unique => false
+    add_index :cart_items, :product_id, :unique => false
+    add_index :cart_items, [:user_id, :product_id], :unique => true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_163521) do
+ActiveRecord::Schema.define(version: 2020_11_23_232330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,17 @@ ActiveRecord::Schema.define(version: 2020_08_28_163521) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "product_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+    t.index ["user_id", "product_id"], name: "index_cart_items_on_user_id_and_product_id", unique: true
+    t.index ["user_id"], name: "index_cart_items_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
